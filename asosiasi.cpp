fbb1cd134b7b2895f0c87224ad116ad24b914015
taufiq72 <taufiqq1207@gmail.com>
2026-05-12 18:27:37 +0700

    deklarasi dan pembuatan class pasien

--- asosiasi.cpp
+++ asosiasi.cpp
@@ -3,3 +3,23 @@
 using namespace std;
 
 class dokter;
+
+
+class pasien {
+public:
+    
+    string nama;
+    vector<dokter*> daftar_dokter;
+
+    pasien(string pNama) :nama(pNama) {
+        cout << "Pasien \"" << nama << "\" ada\n";
+    };
+
+    ~pasien() {
+        cout << "Pasien \"" << nama << "\" tidak ada\n";
+    };
+
+    void tambahDoker(dokter*);
+    void cetakDokter();
+
+};
