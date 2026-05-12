a5328d94582ec3d8cad7d2ee40858ce81c3159ba
taufiq72 <taufiqq1207@gmail.com>
2026-05-12 17:52:39 +0700

    pemanggilan dan pemberian argument pada prosedur tambahAnak()

--- agregasi.cpp
+++ agregasi.cpp
@@ -8,10 +8,14 @@ using namespace std;
 
 int main()
 {
-    //deklarasi dan pemberian nilai variabel pointer untuk objek dari class ibu dan anak
     Ibu* varIbu = new Ibu("rani");
     Ibu* varIbu2 = new Ibu("sari");
     anak* varAnak1 = new anak("tono");
     anak* varAnak2 = new anak("tiara");
     anak* varAnak3 = new anak("dini");
+
+    //pemanggilan dan pemberian argument pada prosedur tambahAnak()
+    varIbu->tambahAnak(varAnak1);
+    varIbu->tambahAnak(varAnak2);
+    varIbu2->tambahAnak(varAnak3);
 }
