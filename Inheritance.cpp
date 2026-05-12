beaa5c6f78ce985a1c06c088ec6f37e2f1c3d40d
taufiq72 <taufiqq1207@gmail.com>
2026-05-12 18:00:55 +0700

    pembuatan class pelajar sebagai pewaris dari class orang

--- Inheritance.cpp
+++ Inheritance.cpp
@@ -2,7 +2,6 @@
 #include <string>
 using namespace std;
 
-//pembuatan class orang
 class orang {
 public:
     string nama;
@@ -17,6 +16,24 @@ public:
 
     int jumlah(int a, int b) {
         return a + b;
+    }
+
+};
+
+class pelajar : public orang {
+public:
+    string sekolah;
+
+    pelajar(string pNama, string pSekolah) :orang(pNama), sekolah(pSekolah) {
+        cout << "Pelajar dibuat\n" << endl;
+    }
+
+    ~pelajar() {
+        cout << "Pelajar dihapus\n" << endl;
+    }
+
+    string perkenalan() {
+        return "Hallo, nama saya " + nama + " dari sekolah " + sekolah + "\n\n";
     }
 
 };
