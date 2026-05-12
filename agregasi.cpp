df22efa7dcd4074c653ba82dd8f63da5b1c56635
taufiq72 <taufiqq1207@gmail.com>
2026-05-12 17:53:06 +0700

    pemanggilan prosedur cetakAnak()

--- agregasi.cpp
+++ agregasi.cpp
@@ -14,8 +14,13 @@ int main()
     anak* varAnak2 = new anak("tiara");
     anak* varAnak3 = new anak("dini");
 
-    //pemanggilan dan pemberian argument pada prosedur tambahAnak()
     varIbu->tambahAnak(varAnak1);
     varIbu->tambahAnak(varAnak2);
     varIbu2->tambahAnak(varAnak3);
+
+    //pemanggilan prosedur cetakAnak()
+    varIbu->cetakAnak();
+    varIbu2->cetakAnak();
+
+    return 0;
 }
