d80846fe981762bc94288182099fef5ad4e95726
taufiq72 <taufiqq1207@gmail.com>
2026-05-12 17:51:26 +0700

    penyertaan file header anak.h dan ibu.h

---
+++ agregasi.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+#include "anak.h"
+#include "ibu.h"
--- ibu.h
+++ ibu.h
@@ -3,14 +3,11 @@
 #include <vector>
 class anak;
 
-//membuat class ibu pada file header ibu.h
 class Ibu {
 public:
-    //deklarasi member variabel pada class ibu
     string nama;
     vector<anak*> daftar_anak;
 
-    //membuat constructor dan destructor pada class ibu
     Ibu(string pNama) :nama(pNama) {
         cout << "Ibu \"" << nama << "\" ada\n";
     };
@@ -19,12 +16,10 @@ public:
         cout << "Ibu \"" << nama << "\" tidak ada\n";
     };
 
-    //deklarasi prosedur tambahAnak() dan cetakAnak()
     void tambahAnak(anak* pAnak);
     void cetakAnak();
 };
 
-//pendefinisian prosedur - prosedur pada class ibu diluar class
 void Ibu::tambahAnak(anak* pAnak) {
     daftar_anak.push_back(pAnak);
 }
