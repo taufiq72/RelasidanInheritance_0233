6384cdd34c1e4edc7e82d6bd2f41598aa1a8ceca
taufiq72 <taufiqq1207@gmail.com>
2026-05-12 18:33:35 +0700

    mendelete variabel - variabel pointer dari memori

--- Jantung.h
+++ Jantung.h
@@ -1,10 +1,8 @@
 #ifndef JANTUNG_H
 #define JANTUNG_H
 
-//membuat class jantung di file header jantung.h
 class Jantung {
 public:
-    //membuat constructor dan destructor dari class jantung
     Jantung() {
         cout << "Jantung Ditambah\n";
     }
@@ -13,4 +11,4 @@ public:
     }
 };
 
-#endif // !JANTUNG_H
+#endif
--- Manusia.h
+++ Manusia.h
@@ -1,14 +1,11 @@
 #ifndef MANUSIA_H
 #define MANUSIA_H
 
-//membuat class manusia di file header manusia.h
 class Manusia {
 public:
-    //membuat member variabel class manusia
     string name;
     Jantung varJantung;
 
-    //membuat constructor dan destructor class manusia
     Manusia(string pName) : name(pName) {
         cout << name << "Hidup\n";
     }
@@ -16,4 +13,4 @@ public:
         cout << name << "Mati\n";
     }
 };
-#endif // !MANUSIA_H
+#endif
