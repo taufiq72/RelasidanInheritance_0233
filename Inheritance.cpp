a60697b1163e5c47d98e10af426d8d41237e97b8
taufiq72 <taufiqq1207@gmail.com>
2026-05-12 17:58:19 +0700

    pembuatan constructor dan destructor dari class orang

--- Inheritance.cpp
+++ Inheritance.cpp
@@ -6,4 +6,13 @@ using namespace std;
 class orang {
 public:
     string nama;
+
+    orang(string pNama) :nama(pNama) {
+        cout << "Orang dibuat\n" << endl;
+    }
+
+    ~orang() {
+        cout << "Orang dihapus\n" << endl;
+    }
+
 };
